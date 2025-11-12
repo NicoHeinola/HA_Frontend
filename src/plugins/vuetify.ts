fb5659381ab1dbbf0587ce0c25c647b7578d3c7f
Nico Heinola <nico.heinola@gmail.com>
2025-11-12 23:16:41 +0200

    feat: update Vuetify configuration to use dark theme and enhance component defaults

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,16 +4,68 @@
  * Framework documentation: https://vuetifyjs.com`
  */
 
-// Composables
-import { createVuetify } from 'vuetify'
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
-import 'vuetify/styles'
+// Composables
+import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
+    defaultTheme: "dark",
+    themes: {
+      dark: {
+        dark: true,
+        colors: {
+          primary: "#FFD600",
+          secondary: "#00CAFF",
+          error: "#FF1744",
+          success: "#00C853",
+
+          surface: "#121212",
+          background: "#0A0A0A",
+        },
+      },
+    },
   },
-})
+  defaults: {
+    VBtn: {
+      variant: "elevated",
+      color: "primary",
+    },
+    VDialog: {
+      width: 800,
+      scrim: "rgba(0,0,0,1)",
+    },
+    VTooltip: {
+      location: "bottom",
+    },
+    VTextarea: {
+      variant: "outlined",
+      color: "secondary",
+      density: "compact",
+    },
+    VTextField: {
+      variant: "outlined",
+      color: "secondary",
+      density: "compact",
+    },
+    VSelect: {
+      variant: "outlined",
+      color: "secondary",
+      density: "compact",
+    },
+    VFileInput: {
+      variant: "outlined",
+      color: "secondary",
+      density: "compact",
+    },
+    VAutocomplete: {
+      variant: "outlined",
+      color: "secondary",
+      density: "compact",
+    },
+  },
+});
