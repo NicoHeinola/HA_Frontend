11a8fd4d341f8f310e488b7c376ef1513469ab70
Nico Heinola <nico.heinola@gmail.com>
2025-11-16 17:54:42 +0200

    feat: separate top and bottom links in navigation bar for improved organization

--- src/components/navigation/NavigationBar.vue
+++ src/components/navigation/NavigationBar.vue
@@ -12,12 +12,16 @@ const links = ref([
     text: "Settings",
     icon: "mdi-cog",
     route: "/settings",
+    bottom: true,
   },
 ]);
 
 const route = useRoute();
-
 const selectedLink = computed(() => route.path);
+
+const topLinks = computed(() => links.value.filter((link) => !link.bottom));
+
+const bottomLinks = computed(() => links.value.filter((link) => link.bottom));
 
 const getLinkColor = (route: string) => {
   return selectedLink.value === route ? "secondary" : "";
@@ -27,7 +31,24 @@ const getLinkColor = (route: string) => {
 <template>
   <v-navigation-drawer :model-value="true" location="left">
     <template #prepend>
-      <template v-for="link in links" :key="link.text">
+      <template v-for="link in topLinks" :key="link.text">
+        <v-list-item
+          class="pa-2"
+          link
+          :class="{ 'active-link': selectedLink === link.route }"
+          :to="link.route"
+          :color="getLinkColor(link.route)"
+        >
+          <template v-slot:prepend>
+            <v-icon :icon="link.icon"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="link.text"></v-list-item-title>
+        </v-list-item>
+      </template>
+    </template>
+    <template #append>
+      <template v-for="link in bottomLinks" :key="link.text">
         <v-list-item
           class="pa-2"
           link
