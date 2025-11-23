c18a4ef9aee39054b78a1a02b76729efdd57879b
Nico Heinola <nico.heinola@gmail.com>
2025-11-23 19:05:24 +0200

    feat: update ESLint configuration and fix package.json formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,16 @@
-import vuetify from 'eslint-config-vuetify'
+import vuetify from "eslint-config-vuetify";
 
-export default vuetify()
+export default vuetify(
+  {},
+  {
+    rules: {
+      "no-var": "error",
+      "prefer-const": "error",
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "always"],
+      "antfu/top-level-function": "off",
+      "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+      "vue/script-indent": "off",
+    },
+  },
+);
