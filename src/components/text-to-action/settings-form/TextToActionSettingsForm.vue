485741a3e8197c8c0731ba0791788836d66135d3
Nico Heinola <nico.heinola@gmail.com>
2025-11-23 18:44:07 +0200

    feat: add functionality to fetch and display available models in TextToActionSettingsForm

--- src/components/text-to-action/settings-form/TextToActionSettingsForm.vue
+++ src/components/text-to-action/settings-form/TextToActionSettingsForm.vue
@@ -2,15 +2,24 @@
 import { defineModel, defineProps } from "vue";
 import TextLabel from "@/components/text-label/TextLabel.vue";
 import { rules } from "./rules";
+import { TextToActionService } from "@/services/text-to-action/TextToAction.service";
+import { useErrorSnackbar } from "@/utils/errorSnackbar";
+import { useSnackbar } from "@/components/use-snackbar/useSnackbar";
 
 const props = defineProps<{
   isLoading?: boolean;
 }>();
 
+const availableModels = ref<string[]>([]);
+const isLoadingModels = ref<boolean>(false);
+
 const settings = defineModel<any[]>({ required: true });
 const isValid = defineModel<boolean>("isValid", { required: false, default: true });
 const allRules = rules();
 const formRef = ref();
+
+const openSnackbar = useSnackbar();
+const { errorSnackbar } = useErrorSnackbar();
 
 const findSettingByKey = (settings: any[], key: string) => {
   return settings.find((s) => s.key === key);
@@ -22,7 +31,22 @@ const updateSettingValue = (settings: any[], key: string, value: string) => {
   setting.value = value;
 };
 
+const getAvailableModels = async () => {
+  isLoadingModels.value = true;
+
+  try {
+    const models = await TextToActionService().getModels();
+    availableModels.value = models["available_models"] || [];
+  } catch (error) {
+    errorSnackbar(error, openSnackbar);
+  } finally {
+    isLoadingModels.value = false;
+  }
+};
+
 onMounted(async () => {
+  await getAvailableModels();
+
   const isFormValid = (await formRef.value?.validate()) ?? true;
 
   isValid.value = isFormValid.valid;
@@ -32,6 +56,16 @@ onMounted(async () => {
 <template>
   <v-form ref="formRef" v-model="isValid">
     <v-row>
+      <v-col cols="12">
+        <v-select
+          :items="availableModels"
+          :model-value="findSettingByKey(settings, 'default_model')?.value"
+          :loading="isLoadingModels || props.isLoading"
+          label="Default Model"
+          @update:model-value="(update) => updateSettingValue(settings, 'default_model', update)"
+          :rules="allRules['default_model']"
+        ></v-select>
+      </v-col>
       <v-col cols="12">
         <v-number-input
           :model-value="Number(findSettingByKey(settings, 'prediction_timeout')?.value)"
