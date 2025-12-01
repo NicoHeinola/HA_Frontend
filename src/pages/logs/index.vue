63d17cf4c6a9ff5ba291597e21edcdc840410034
Nico Heinola <nico.heinola@gmail.com>
2025-12-01 23:41:12 +0200

    feat: implement auto-refresh functionality for logs with toggle button

--- src/pages/logs/index.vue
+++ src/pages/logs/index.vue
@@ -5,6 +5,8 @@ import { useSnackbar } from "@/components/use-snackbar/useSnackbar";
 
 const logs = ref<Array<string>>([]);
 const isLoading = ref<boolean>(false);
+const autoRefreshEnabled = ref<boolean>(true);
+const refreshTimer = ref<number | null>(null);
 
 const openSnackbar = useSnackbar();
 const { errorSnackbar } = useErrorSnackbar();
@@ -12,6 +14,7 @@ const { errorSnackbar } = useErrorSnackbar();
 const logServices = ref({
   tta: { value: "tta", fn: LogService().getTextToActionLogs, title: "TTA Logs" },
 });
+
 const selectedLogService = ref(logServices.value.tta);
 
 const fetchLogs = async () => {
@@ -32,8 +35,35 @@ const fetchLogs = async () => {
   }
 };
 
+const startAutoRefresh = () => {
+  refreshTimer.value = setInterval(() => {
+    fetchLogs();
+  }, 1000);
+};
+
+const stopAutoRefresh = () => {
+  if (refreshTimer.value) {
+    clearInterval(refreshTimer.value);
+    refreshTimer.value = null;
+  }
+};
+
+const toggleAutoRefresh = () => {
+  autoRefreshEnabled.value = !autoRefreshEnabled.value;
+  if (autoRefreshEnabled.value) {
+    startAutoRefresh();
+  } else {
+    stopAutoRefresh();
+  }
+};
+
 onMounted(() => {
   fetchLogs();
+  startAutoRefresh();
+});
+
+onUnmounted(() => {
+  stopAutoRefresh();
 });
 </script>
 
@@ -44,16 +74,16 @@ onMounted(() => {
         <h1>Logs</h1>
         <div class="d-flex ga-2 align-center">
           <v-select label="Log Type" :items="Object.values(logServices)" v-model="selectedLogService" return-object />
+          <v-btn color="primary" @click="toggleAutoRefresh" :variant="autoRefreshEnabled ? 'tonal' : 'outlined'">
+            {{ autoRefreshEnabled ? "Stop Auto Refresh (1s)" : "Start Auto Refresh" }}
+          </v-btn>
           <v-btn color="primary" @click="fetchLogs" :loading="isLoading"> Refresh Logs </v-btn>
         </div>
       </v-col>
     </v-row>
 
     <v-row class="d-flex overflow-hidden">
-      <v-col cols="12" v-if="isLoading">
-        <v-skeleton-loader type="paragraph" />
-      </v-col>
-      <v-col cols="12" class="h-100 w-100" v-else>
+      <v-col cols="12" class="h-100 w-100">
         <code-block>
           {{ logs.join("\n") }}
         </code-block>
